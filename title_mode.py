843937029da11e13a08a2cff014708492051d700
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-30 11:53:41 +0900

    모드 간의 이동구현 다만 play_mode로 들어갈 때 에러 발생
    play_mode, 게임월드에 모드를 들어갔다가 나올 때 계속해서 boy 객체들이 생김
    play_mode에서 나갈 때 게임 월드를 초기화 해주면됨

--- title_mode.py
+++ title_mode.py
@@ -1,7 +1,9 @@
 from pico2d import load_image, clear_canvas, update_canvas, get_events, get_time
-from sdl2 import SDL_KEYDOWN, SDLK_ESCAPE
+from sdl2 import SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
 
 import game_framework
+import play_mode
+
 
 def init():
     global image
@@ -23,3 +25,5 @@ def handle_events():
     for event in events:
         if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(play_mode)
