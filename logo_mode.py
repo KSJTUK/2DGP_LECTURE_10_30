165bfb30f27030e833783f776754e387e58cba61
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-30 11:19:46 +0900

    2초가 지나면 로고 모드가 종료되도록 하는 코드 추가

--- logo_mode.py
+++ logo_mode.py
@@ -1,18 +1,22 @@
-from pico2d import load_image, clear_canvas, update_canvas, get_events
+from pico2d import load_image, clear_canvas, update_canvas, get_events, get_time
 
 
 def init():
     global image
     global running
+    global logo_start_time
 
     running = True
     image = load_image('tuk_credit.png')
+    logo_start_time = get_time()
 
 def finish():
     pass
 
 def update():
-    pass
+    global running
+    if get_time() - logo_start_time >= 2.0:
+        running = False
 
 def draw():
     clear_canvas()
