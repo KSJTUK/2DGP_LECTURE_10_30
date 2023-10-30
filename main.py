84f4b742945df1110dc537a0435f5b7c82a4e4ca
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-30 11:41:58 +0900

    타이틀 모드를 생성하고 실행하도록 변경

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pico2d import open_canvas, close_canvas
 import logo_mode
+import title_mode as start_mode
 import game_framework
 
 open_canvas()
-game_framework.run(logo_mode)
+game_framework.run(start_mode)
 close_canvas()
